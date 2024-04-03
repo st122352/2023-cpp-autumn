0793a7e7ce3e6afd7826b24ce3d99443bbb1e061
st122352 <kuklin.ilya.27@mail.ru>
2024-04-03 22:28:12 +0300

    Homework 1 code upload

---
+++ 2024.04.02-homework-1/Project5/5.cpp
@@ -0,0 +1,301 @@
+#include<iostream>
+
+struct SEdge {
+	int a;
+	int b;
+	int w;
+	SEdge(int a = 0, int b = 0, int w = 1) : a(a), b(b), w(w) {}
+	SEdge(const SEdge& src) : a(src.a), b(src.b), w(src.w) {}
+	~SEdge() {}
+	void set(int a, int b, int w)
+	{
+		this->a = a;
+		this->b = b;
+		this->w = w;
+	}
+	friend std::ostream& operator<<(std::ostream& stream, const SEdge& edge);
+};
+
+class CGraph {
+public:
+	CGraph();
+	CGraph(int vertexes, int edges, int t);
+	~CGraph();
+	///выводит матрицу смежности графа
+	void PrintMatrix();
+	///выводит дуги графа
+	void PrintEdges();
+	///считывает матрицу смежности графа
+	void ReadMatrix(int vertexes, std::istream& stream);
+	///считывает дуги графа
+	void ReadEdges(int edges, std::istream& stream, bool haveweight = false);
+	int evolutionTree(int n);
+	int findClosestAncestor(int s1, int s2);
+
+
+private:
+	///создает матрицу смежности n*n и матрицу с дугами размера m
+	void init();
+	///создает матрицу смежности _vertexes*_vertexes
+	void initMatrix();
+	///создает матрицу матрицу с дугами размера _edges
+	void initEdges();
+	void initMatrixFromEdges();
+	void initEdgesFromMatrix();
+	int getVertexesCountFromEdges();
+	int getEdgesCountFromMatrix();
+	///удаляет матрицу смежности и матрицу с дугами
+	void dispose();
+	///удаляет матрицу смежности
+	void disposeMatrix();
+	///удаляет матрицу с дугами
+	void disposeEdges();
+
+	int _vertexes;
+	int _edges;
+	int** _matrix;
+	SEdge* _edge;
+	int t;
+};
+
+int main(int argc, char* argv[])
+{
+	int t = 0;
+	int s1 = 0;
+	int s2 = 0;
+	std::cin >> t;
+	std::cin >> s1;
+	std::cin >> s2;
+
+	CGraph evolution(0, 0, t);
+	int closestAn = evolution.findClosestAncestor(s1, s2);
+
+	std::cout << closestAn << std::endl;
+
+	return EXIT_SUCCESS;
+}
+
+CGraph::CGraph()
+	: _vertexes(0), _edges(0), _matrix(nullptr), _edge(nullptr) {}
+
+CGraph::CGraph(int vertexes, int edges, int t)
+	: _vertexes(vertexes), _edges(edges), _matrix(nullptr), _edge(nullptr), t(t)
+{
+	init();
+}
+
+CGraph::~CGraph()
+{
+	dispose();
+}
+
+void CGraph::PrintMatrix()
+{
+	if (_matrix == nullptr)
+	{
+		if (_edge == nullptr)
+		{
+			std::cout << "Graph empty" << std::endl;
+			return;
+		}
+		initMatrixFromEdges();
+	}
+	for (int i = 1; i < _vertexes; ++i)
+	{
+		for (int j = 1; j < _vertexes; ++j)
+		{
+			std::cout << _matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void CGraph::PrintEdges()
+{
+	if (_edge == nullptr)
+	{
+		if (_matrix == nullptr)
+		{
+			std::cout << "Graph empty" << std::endl;
+			return;
+		}
+		initEdgesFromMatrix();
+	}
+	for (int i = 0; i < _edges; ++i)
+	{
+		std::cout << _edge[i] << std::endl;
+	}
+}
+
+void CGraph::ReadMatrix(int vertexes, std::istream& stream)
+{
+	_vertexes = vertexes;
+	initMatrix();
+	for (int i = 0; i < _vertexes; ++i)
+	{
+		for (int j = 0; j < _vertexes; ++j)
+		{
+			stream >> _matrix[i][j];
+		}
+	}
+	initEdgesFromMatrix();
+}
+
+void CGraph::ReadEdges(int edges, std::istream& stream, bool haveweight)
+{
+	_edges = edges;
+	initEdges();
+	for (int i = 0; i < _edges; ++i)
+	{
+		std::cin >> _edge[i].a >> _edge[i].b;
+		if (haveweight)
+		{
+			std::cin >> _edge[i].w;
+		}
+	}
+}
+
+void CGraph::init()
+{
+	dispose();
+	initMatrix();
+	initEdges();
+}
+
+void CGraph::initMatrix()
+{
+	if (_vertexes == 0)
+	{
+		return;
+	}
+	_matrix = new int* [_vertexes];
+	for (int i = 0; i < _vertexes; ++i)
+	{
+		_matrix[i] = new int[_vertexes] { 0 };
+	}
+}
+
+void CGraph::initEdges()
+{
+	if (_edges == 0)
+	{
+		return;
+	}
+	_edge = new SEdge[_edges];
+}
+
+void CGraph::initMatrixFromEdges()
+{
+	disposeMatrix();
+	_vertexes = getVertexesCountFromEdges();
+	initMatrix();
+	for (int i = 0; i < _edges; ++i)
+	{
+		_matrix[_edge[i].a][_edge[i].b] = _edge[i].w;
+	}
+}
+
+int CGraph::getEdgesCountFromMatrix()
+{
+	int count = 0;
+	for (int i = 0; i < _vertexes; ++i)
+	{
+		for (int j = 0; j < _vertexes; ++j)
+		{
+			count += (_matrix[i][j] != 0);
+		}
+	}
+	return count;
+}
+
+void CGraph::dispose()
+{
+	disposeMatrix();
+	disposeEdges();
+}
+
+void CGraph::disposeMatrix()
+{
+	if (_matrix != nullptr)
+	{
+		for (int i = 0; i < _vertexes; ++i)
+		{
+			delete[] _matrix[i];
+		}
+		delete[] _matrix;
+		_matrix = nullptr;
+	}
+}
+
+void CGraph::disposeEdges()
+{
+	if (_edge != nullptr)
+	{
+		delete[] _edge;
+		_edge = nullptr;
+	}
+}
+
+void CGraph::initEdgesFromMatrix()
+{
+	disposeEdges();
+	_edges = getEdgesCountFromMatrix();
+	initEdges();
+	for (int i = 0, k = 0; i < _vertexes && k < _edges; ++i)
+	{
+		for (int j = 0; j < _vertexes && k < _edges; ++j)
+		{
+			if (_matrix[i][j] != 0)
+			{
+				_edge[k++].set(i + 1, j + 1, _matrix[i][j]);
+			}
+		}
+	}
+}
+
+int CGraph::getVertexesCountFromEdges()
+{
+	int res = 0;
+	for (int i = 0; i < _edges; ++i)
+	{
+		res = (res > _edge[i].a ? res : _edge[i].a);
+		res = (res > _edge[i].b ? res : _edge[i].b);
+	}
+	return res + 1;
+}
+
+std::ostream& operator<<(std::ostream& stream, const SEdge& edge)
+{
+	stream << edge.a << " " << edge.b;
+	if (edge.w > 1)
+	{
+		stream << " " << edge.w;
+	}
+	return stream;
+}
+
+
+
+int CGraph::evolutionTree(int n)
+{
+	t = n;
+	return t;
+}
+
+int CGraph::findClosestAncestor(int s1, int s2)
+{
+	while (s1 != s2)
+	{
+		if (s1 < s2)
+		{
+			s2 = s2 / 2;
+		}
+		else
+		{
+			s1 = s1 / 2;
+		}
+	}
+
+	return s1;
+
+}
